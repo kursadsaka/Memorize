d04dae2fa3e1b6ea55b2276c051f43ef46bd302a
Kürşad Saka <kursadsakaa@gmail.com>
2022-02-24 01:00:00 +0300

    add scale animation on game start

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -24,20 +24,38 @@ struct EmojiMemoryGameView: View {
         
     }
     
+    @State private var dealt = Set<Int>()
+    
+    private func deal(_ card: EmojiMemoryGame.Card) {
+        dealt.insert(card.id)
+    }
+    
+    private func isUndealt(_ card: EmojiMemoryGame.Card) -> Bool {
+        !dealt.contains(card.id)
+    }
+    
     var gameBody: some View {
         
         AspectVGrid(items: game.cards, aspectRatio: 2/3) {card in
-            if card.isMatched && !card.isFaceUp {
+            if isUndealt(card) || (card.isMatched && !card.isFaceUp) {
                 Color.clear
             }
             else {
                 CardView(card: card)
                     .padding(4)
+                    .transition(AnyTransition.asymmetric(insertion: .scale, removal: .opacity).animation(.easeInOut))
                     .onTapGesture {
                         withAnimation {
                             game.choose(card)
                         }
                     }
+            }
+        }
+        .onAppear {
+            withAnimation {
+                for card in game.cards {
+                    deal(card)
+                }
             }
         }
         .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
