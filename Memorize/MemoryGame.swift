56f254cc3f561d7671e23218ae3e2f28405d5f8e
Kürşad Saka <kursadsakaa@gmail.com>
2022-02-27 04:42:05 +0300

    add pie animation

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -49,10 +49,80 @@ struct MemoryGame<CardContent: Equatable> {
     }
     
     struct Card: Identifiable {
-        var isFaceUp = false
-        var isMatched = false
+        
+        var isFaceUp = false {
+            didSet {
+                if isFaceUp {
+                    startUsingBonusTime()
+                }
+                else {
+                    stopUsingBonusTime()
+                }
+            }
+        }
+        
+        var isMatched = false {
+            didSet {
+                stopUsingBonusTime()
+            }
+        }
+        
         let content: CardContent
         let id: Int
+        
+        // MARK: - Bonus Time
+        
+        // this could give matching bonus points
+        // if the user matches card
+        // before a certain amount of time passes during which card is face up
+        
+        // can be zero which means "no bonus available" for this card
+        var bonusTimeLimit: TimeInterval = 6
+        
+        // how long this card has ever been face up
+        private var faceUpTime: TimeInterval {
+            if let lastFaceUpDate = self.lastFaceUpDate {
+                return pastFaceUpTime + Date().timeIntervalSince(lastFaceUpDate)
+            }
+            else {
+                return pastFaceUpTime
+            }
+        }
+        
+        // the last time this card was turned face up (and is still face up)
+        var lastFaceUpDate: Date?
+        
+        // the accumulated time this card has been face up in the past
+        // (i.e. not including the current time it's been face up if it is currently so)
+        var pastFaceUpTime: TimeInterval = 0
+        
+        var bonusTimeRemaining: TimeInterval {
+            max(0, bonusTimeLimit - faceUpTime)
+        }
+        
+        var bonusRemaining: Double {
+            (bonusTimeLimit > 0 && bonusTimeRemaining > 0) ? bonusTimeRemaining/bonusTimeLimit : 0
+        }
+        
+        var hasEarnedBonus: Bool {
+            isMatched && bonusTimeRemaining > 0
+        }
+        
+        var isConsumingBonusTime: Bool {
+            isFaceUp && !isMatched && bonusTimeRemaining > 0
+        }
+        
+        private mutating func startUsingBonusTime() {
+            if isConsumingBonusTime, lastFaceUpDate == nil {
+                lastFaceUpDate = Date()
+            }
+        }
+        
+        private mutating func stopUsingBonusTime() {
+            pastFaceUpTime = faceUpTime
+            self.lastFaceUpDate = nil
+        }
+        
     }
     
 }
